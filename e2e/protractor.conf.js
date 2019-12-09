3608d52c81d8d94a3038b203cc57b16d4c67c629
Juri Boiko <jboiko@williamhill.co.uk>
2019-12-09 16:28:02 +0100

    first

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -7,7 +7,10 @@ exports.config = {
   allScriptsTimeout: 11000,
   specs: ['./src/features/**/*.feature'],
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
