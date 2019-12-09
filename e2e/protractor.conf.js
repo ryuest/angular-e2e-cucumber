64c9544ee4326dd701694bea0211fab56a506040
Juri Boiko <jboiko@williamhill.co.uk>
2019-12-09 14:42:26 +0100

    first

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,6 +1,8 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
+var reporter = require('cucumber-html-reporter');
+
 exports.config = {
   allScriptsTimeout: 11000,
   specs: ['./src/features/**/*.feature'],
@@ -12,11 +14,32 @@ exports.config = {
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
   cucumberOpts: {
-    require: ['./src/steps/**/*.steps.ts']
+    format: 'json:./e2e/report/cucumberreport.json',
+    require: ['./src/steps/**/*.ts']
   },
+  SELENIUM_PROMISE_MANAGER: false,
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.json')
     });
+  },
+  onComplete() {
+    var options = {
+      theme: 'bootstrap',
+      jsonFile: './e2e/report/cucumberreport.json',
+      output: './e2e/report/cucumber_report.html',
+      reportSuiteAsScenarios: true,
+      scenarioTimestamp: true,
+      launchReport: true,
+      metadata: {
+          "App Version":"0.3.2",
+          "Test Environment": "STAGING",
+          "Browser": "Chrome  54.0.2840.98",
+          "Platform": "Windows 10",
+          "Parallel": "Scenarios",
+          "Executed": "Remote"
+      }
+  };
+  reporter.generate(options);
   }
 };
